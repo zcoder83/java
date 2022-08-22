cfe13c31fef36de0bcd744e85cf437242dbff32d
Yeon <dusrksl@gmail.com>
2022-08-22 15:17:13 -0400

    start mini_banking

---
+++ LearnJava/src/com/Yeon/Yeon.java
@@ -0,0 +1,10 @@
+package com.Yeon;
+
+public class Yeon {
+    public static void callMyName(){
+        System.out.println("Yeonkwan Jeong");
+    }
+    public static void callMyAge(){
+        System.out.println(39);
+    }
+}
---
+++ test/Yeon.java
@@ -0,0 +1,9 @@
+public class Yeon {
+	public static void callMyName(){
+		System.out.println("Yeonkwan Jeong");
+	}
+	public static void callMyAge(){
+		System.out.println(39);
+	}
+}
+
