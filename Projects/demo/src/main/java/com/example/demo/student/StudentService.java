2ea76cf24ebedb750965053a3a32154fafb70755
Yeon <dusrksl@gmail.com>
2022-09-27 23:53:03 -0400

    completed DELETE process in JPA

--- Projects/demo/src/main/java/com/example/demo/student/StudentService.java
+++ Projects/demo/src/main/java/com/example/demo/student/StudentService.java
@@ -30,4 +30,12 @@ public class StudentService {
         }
         studentRepository.save(student);
     }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists) {
+            throw new IllegalStateException("student with id " + studentId + " does not exist");
+        }
+        studentRepository.deleteById(studentId);
+    }
 }
