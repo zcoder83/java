12d665f84c8cd346e1f7f1f48aa6d7b7b7fd06c8
Yeon <dusrksl@gmail.com>
2022-09-29 23:33:13 -0400

    free version finished in JPA course

--- Projects/spring-data-jpa-course/src/main/java/com/example/demo/Application.java
+++ Projects/spring-data-jpa-course/src/main/java/com/example/demo/Application.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +12,11 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student yeon = new Student("yeon", "jeong", "yeon@gmail.com", 21);
+            studentRepository.save(yeon);
+        };
+    }
 }
