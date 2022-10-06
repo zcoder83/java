c51de36e6980a0ef612691d73a84719c3fcb71bb
Yeon <dusrksl@gmail.com>
2022-10-05 22:24:21 -0400

    set router and added header and footer

--- Projects/React-Springboot-CRUD-App/react-frontend/src/App.js
+++ Projects/React-Springboot-CRUD-App/react-frontend/src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import FooterComponent from "./components/FooterComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import ListEmployeeComponent from "./components/ListEmployeeComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>  
-      </header>
+    <div>
+      <Router>
+          <HeaderComponent />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<ListEmployeeComponent />}></Route>
+              <Route path="/employees" element={<ListEmployeeComponent />}></Route>
+            </Routes>
+          </div>
+          <FooterComponent />
+      </Router>
     </div>
   );
 }
